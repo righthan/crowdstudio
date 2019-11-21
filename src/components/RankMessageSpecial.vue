6591e73750202a61be301ce59be2744eb3dca2f9
Jae Hwan Jeong <jeong7331@Jaes-MacBook-Pro-2.local>
2019-11-21 15:56:26 +0900

    rank generic layout

--- src/components/RankMessageSpecial.vue
+++ src/components/RankMessageSpecial.vue
@@ -1,10 +1,55 @@
 <template>
   <v-card
-    class="pa-2"
+    class="pa-2 text-center title"
     tile
     style="height: 300px;"
   >
-    RankMessageSpecial
+    Ranks
+    
+      <v-col class="text-center">
+        <div class="my-2 px-0">
+          <v-row align="center">
+            <div class="my-2 pr-2">
+              <h1 class="purple--text"> 1 </h1>
+            </div>
+            <div class="my-2 px-0 pr-1" style="width: 250px;">
+              <v-btn text block outlined color="purple">First</v-btn>
+            </div>
+            <v-btn text icon color="deep-purple">
+              <v-icon>mdi-thumb-up</v-icon>
+            </v-btn>
+          </v-row>
+        </div>
+
+        <div class="my-2">
+          <v-row align="center">
+            <div class="my-2 pr-3">
+              <h2 class="purple--text"> 2 </h2>
+            </div>
+            <div class="my-2 px-0 pr-1" style="width: 250px;">
+              <v-btn text block outlined color="purple">Second</v-btn>
+            </div>
+            <v-btn text icon color="deep-purple">
+              <v-icon>mdi-thumb-up</v-icon>
+            </v-btn>
+          </v-row>
+        </div>
+        
+        <div class="my-2">
+          <v-row align="center">
+            <div class="my-2 pr-4">
+              <h3 class="purple--text"> 3 </h3>
+            </div>
+            <div class="my-2 px-0 pr-1" style="width: 250px;">
+              <v-btn text block outlined color="purple"> Third</v-btn>
+            </div>
+            <v-btn text icon color="deep-purple">
+              <v-icon>mdi-thumb-up</v-icon>
+            </v-btn>
+          </v-row>
+        </div>
+      </v-col>
+    
   </v-card>
 </template>
 
