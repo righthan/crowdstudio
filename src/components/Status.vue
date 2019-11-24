c4ae3e84e2cf143213e15a2b05c5beb4d4d3f104
김석환 <seokhwankim@gimseoghwan-ui-MacBookAir.local>
2019-11-25 00:04:41 +0900

    delete message

--- src/components/Status.vue
+++ src/components/Status.vue
@@ -5,34 +5,29 @@
         style="height: 300px;"
     >
         Message Status
-    <v-divider :inset="inset"></v-divider>
-        <v-container>
-            <v-row class="mt-8 mb-8 ml-1">
-                <v-col cols="2">
-                    <div class="text-center title grey--text">{{message.rank}}</div>
-                    <div class="text-center"><v-icon large justify-center color="purple">{{message.arrow}}</v-icon></div>
-
-                </v-col>
-                <v-col cols="9">
-                    <v-card color="#F3E5F5"><v-card-text>{{message.content}}</v-card-text></v-card>
-
-                </v-col>
-            </v-row>
-            <v-row justify="center">
-                    <v-btn class="white--text" color="red">Delete<v-icon right>mdi-delete</v-icon></v-btn>
-            </v-row>
-        </v-container>
-    
+        <v-divider :inset="inset"></v-divider>
+        <UserMessageSpecial/>
     </v-card>
 
 </template>
 
 <script>
+import UserMessageSpecial from '../components/UserMessageSpecial.vue'
 export default {
   name: 'Status',
+  components:{
+      UserMessageSpecial
+    },
   data: () =>({
       message: {content:"Message Message Message Message Message Message", rank:"10", arrow:"mdi-arrow-up"}
-  })
-
+  }),
+    methods:{
+        DeleteMessage(){
+            document.getElementById("rank").remove()
+            document.getElementById("message").remove()
+            document.getElementById("dbtn").remove()
+            
+        }
+    }
 }
 </script>
---
+++ src/components/UserMessageSpecial.vue
@@ -0,0 +1,39 @@
+<template>
+
+    <v-container>
+        <v-row class="mt-8 mb-8 ml-1">
+            <v-col id="rank" cols="2">
+                <div class="text-center title grey--text">{{message.rank}}</div>
+                <div class="text-center"><v-icon large justify-center color="purple">{{message.arrow}}</v-icon></div>
+
+            </v-col>
+            <v-col id="message" cols="9">
+                <v-card color="#F3E5F5"><v-card-text>{{message.content}}</v-card-text></v-card>
+
+            </v-col>
+        </v-row>
+        <v-row justify="center">
+                <v-btn id="dbtn"  @click="DeleteMessage()" class="white--text" color="red">Delete<v-icon right>mdi-delete</v-icon></v-btn>
+        </v-row>
+    </v-container>
+
+
+
+</template>
+
+<script>
+export default {
+  name: 'Status',
+  data: () =>({
+      message: {content:"Message Message Message Message Message Message", rank:"10", arrow:"mdi-arrow-up"}
+  }),
+    methods:{
+        DeleteMessage(){
+            document.getElementById("rank").remove()
+            document.getElementById("message").remove()
+            document.getElementById("dbtn").remove()
+            
+        }
+    }
+}
+</script>
