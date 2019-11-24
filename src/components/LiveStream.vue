8f4c02aabd9f3534319e8c1db2d67b987f3c833f
righthan <hanuright@gmail.com>
2019-11-24 20:21:59 +0900

    embed video + help

--- src/components/LiveStream.vue
+++ src/components/LiveStream.vue
@@ -5,18 +5,36 @@
     outlined
     style="height: 500px;"
   >
-    LiveStream
-    <help/>
+    <iframe src="https://player.twitch.tv/?channel=tvcrank" frameborder="0" allowfullscreen="true" scrolling="no" height="85%" width="100%"></iframe>
+    <v-divider/>
+    <v-row>
+      <v-col cols="6" align-self="start" class = "video_info">
+        <h2>
+          We are the crowdstudio!
+        </h2>
+        <a href="https://youtube.com">Yollllo</a>
+      </v-col>
+      <v-col cols="6">
+        <span style="float: right;">
+          <v-icon>
+            mdi-account
+          </v-icon>
+          1,000 viewers
+        </span>
+      </v-col>
+    </v-row>
   </v-card>
 </template>
 
 <script>
-import Help from './Help.vue'
-
 export default {
-  name: 'LiveStream',
-  components: {
-    Help,
-  }
+  name: 'LiveStream'
 }
 </script>
+
+<style scoped>
+.video_info {
+  padding-top: 0px;
+  padding-bottom: 0px;
+}
+</style>
