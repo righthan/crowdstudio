ed37471e4f3c0011d6a41180cfd1b6b46ac6b21e
righthan <hanuright@gmail.com>
2019-11-25 19:26:28 +0900

    [merge] sendMessage add sender

--- src/components/SendMessage.vue
+++ src/components/SendMessage.vue
@@ -5,8 +5,35 @@
         class="pa-2"
         tile
         outlined
-        style="height: 200px;"
-      >
+        style="height: 220px;"
+      >        
+        <v-row class="mt-2 mb-2" justify=center align=center>
+          <v-col cols="9">
+            <v-hover v-slot:default="{ hover }">
+              <v-card color="#F3E5F5">
+                
+                <v-expand-transition>
+                  <div
+                    v-if="hover"
+                    class="d-flex transition-fast-in-fast-out white darken-2 v-card--reveal display-3"
+                    style="height: 100%;"
+                  >
+                    <v-btn text icon color="deep-purple">
+                       <v-icon>mdi-thumb-up</v-icon>
+                    </v-btn>
+                  </div>
+                </v-expand-transition>
+
+                <v-card-text>Message Message Message Message Message Message</v-card-text>
+                
+                </v-card>
+            </v-hover>
+          </v-col>
+          
+        </v-row>
+
+        <v-divider :inset="inset"></v-divider>
+
         {{usrMessage}}
         <v-textarea
           v-model="message"
@@ -45,6 +72,24 @@ export default {
       }
     },
   },
+
+  methods: {
+    OnKeydown: function (event) {
+      if (event.key === "Tab") {
+        event.preventDefault()
+        this.isTargetViewer = !this.isTargetViewer
+      }
+      else if (event.key === "Enter" && event.shiftKey) {
+        this.sendMessage()
+      }
+    },
+    sendMessage: function () {
+      this.message = ''
+      console.log("TEMP: Send a message")
+    }
+  }
+}
+</script>
 
   methods: {
     OnKeydown: function (event) {
