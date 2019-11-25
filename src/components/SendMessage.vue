aee76056e0712c9bed8071627f857a07f95feb16
righthan <hanuright@gmail.com>
2019-11-25 18:21:34 +0900

    [merge] temp store for merge

--- src/components/SendMessage.vue
+++ src/components/SendMessage.vue
@@ -7,7 +7,19 @@
         outlined
         style="height: 200px;"
       >
-        Send Message
+        {{usrMessage}}
+        <v-textarea
+          v-model="message"
+          append-outer-icon='mdi-send'
+          @click:append-outer="sendMessage"
+          @keydown="OnKeydown"
+          autofocus
+          :label="usrMessage"
+          outlined
+          filled
+          rows="3"
+        />
+
       </v-card>
     </v-col>
   </v-row>
@@ -16,5 +28,38 @@
 <script>
 export default {
   name: 'SendMessage',
+  data: function () {
+    return {
+      isTargetViewer: true,
+      message: '', 
+    }
+  },
+
+  computed: {
+    usrMessage: function () {
+      if (this.isTargetViewer === true) {
+        return "> All viewers (Press 'tab' to send to streamer)"
+      }
+      else {
+        return "> Streamer (Press 'tab' to send to all viewers)"
+      }
+    },
+  },
+
+  methods: {
+    OnKeydown: function (event) {
+      if (event.key === "Tab") {
+        event.preventDefault()
+        this.isTargetViewer = !this.isTargetViewer
+      }
+      else if (event.key === "Enter" && event.shiftKey) {
+        this.sendMessage()
+      }
+    },
+    sendMessage: function () {
+      this.message = ''
+      console.log("TEMP: Send a message")
+    }
+  }
 }
 </script>
