c05aeb8cc8eb36a459cda0b79af5ebfcc7a323a4
tsook <kimts0830@gmail.com>
2019-11-25 21:33:24 +0900

    Connect with backend to show/hide current message if it exists/not

--- src/components/UserMessageSpecial.vue
+++ src/components/UserMessageSpecial.vue
@@ -3,12 +3,12 @@
     <v-container>
         <v-row class="mt-8 mb-8 ml-1">
             <v-col id="rank" cols="2">
-                <div class="text-center title grey--text">{{message.rank}}</div>
-                <div class="text-center"><v-icon large justify-center color="purple">{{message.arrow}}</v-icon></div>
+                <div class="text-center title grey--text">{{message ? message.rank : ""}}</div>
+                <div class="text-center"><v-icon large justify-center color="purple">{{message ? message.arrow : ""}}</v-icon></div>
 
             </v-col>
             <v-col id="message" cols="9">
-                <v-card color="#F3E5F5"><v-card-text>{{message.content}}</v-card-text></v-card>
+                <v-card color="#F3E5F5"><v-card-text>{{message ? message.content : ""}}</v-card-text></v-card>
 
             </v-col>
         </v-row>
@@ -26,25 +26,39 @@ export default {
   name: 'Status',
   props: ['socket'],
   data: () =>({
-      message: {content:"Message Message Message Message Message Message", rank:"10", arrow:"mdi-arrow-up"}
+      message: null
   }),
   mounted() {
     this.socket.on("msg status update", (msg) => {
       if(msg == null){
         // if no current message
-        msg
+        this.HideMessage()
       }else{
         // check if new rank is less or more than old rank
-        this.message = {content: msg.content, rank: msg.rank, arrow:"mdi-arrow-up"}
+        this.ShowMessage()
+        var arrow = "mdi-minus"
+        if(this.message && msg.rank > this.message.rank)
+            arrow = "mdi-arrow-up"
+        else if(this.message && msg.rank < this.message.rank)
+            arrow = "mdi-arrow-down"
+        this.message = {content: msg.content, rank: msg.rank, arrow: arrow}
       }
     })
   },
     methods:{
         DeleteMessage(){
-            document.getElementById("rank").remove()
-            document.getElementById("message").remove()
-            document.getElementById("dbtn").remove()
-            
+            this.HideMessage()
+            this.socket.emit("delete special")
+        },
+        HideMessage(){
+            document.getElementById("rank").hidden = true
+            document.getElementById("message").hidden = true
+            document.getElementById("dbtn").hidden = true
+        },
+        ShowMessage(){
+            document.getElementById("rank").hidden = false
+            document.getElementById("message").hidden = false
+            document.getElementById("dbtn").hidden = false
         }
     }
 }
